dd01cc4b8b9fd74d7df93a15781cc3a10a8ff853
Stefan Buhrmester <buhrmi@gmail.com>
2022-06-19 20:20:47 +0200

    default locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_active_storage
+  before_action :get_locale
 
   inertia_share do 
     {
@@ -8,6 +9,17 @@ class ApplicationController < ActionController::Base
       header_menu: Spina::Navigation.find_by(name: 'header')&.navigation_items&.map { |i| {path: i.materialized_path, label: i.menu_title || "[#{I18n.locale.to_s.upcase} MISSING]"}} || [],
       footer_menu: Spina::Navigation.find_by(name: 'footer')&.navigation_items&.map { |i| {path: i.materialized_path, label: i.menu_title || "[#{I18n.locale.to_s.upcase} MISSING]"}} || [],
     } 
+  end
+
+  def get_locale
+    if params[:locale].present?
+      I18n.locale = params[:locale]
+      cookies[:locale] = params[:locale]
+    elsif cookies[:locale].present?
+      I18n.locale = params[:locale] = cookies[:locale]
+    else
+      I18n.locale = params[:locale] = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
   end
 
   def spina_content
