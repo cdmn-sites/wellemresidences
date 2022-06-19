3ecb59645739e83744df329f70d095bd8b863297
Stefan Buhrmester <buhrmi@gmail.com>
2022-06-19 19:47:37 +0200

    room types

--- app/controllers/spina/admin/room_types_controller.rb
+++ app/controllers/spina/admin/room_types_controller.rb
@@ -6,16 +6,18 @@ module Spina::Admin
     admin_section :content
 
     def index
-      @room_types = RoomType.all
+      @room_types = RoomType.all.page(params[:page]).per(25)
     end
 
     def edit
+      add_breadcrumb 'Room Types', Rails.application.routes.url_helpers.admin_room_types_path
+      add_breadcrumb @room_type.name
     end
 
     def update
       if @room_type.update(room_type_params)
         # redirect_to spina.edit_admin_layout_path(locale: @locale), flash: {success: t('spina.layout.saved')}
-        redirect_back fallback_location: spina.edit_admin_layout_path(locale: @locale), flash: {success: t('spina.layout.saved')}
+        redirect_back fallback_location: spina.edit_admin_layout_path(locale: @locale), flash: {success: 'Room Type Saved'}
       else
         flash.now[:error] = t('room_type.couldnt_be_saved')
         render partial: 'error', status: :unprocessable_entity
