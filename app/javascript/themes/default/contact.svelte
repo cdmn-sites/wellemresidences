58615153cd1df4f3b8dd50a8e1c7a5b8df1ed195
Stefan Buhrmester <buhrmi@gmail.com>
2022-06-20 16:25:28 +0200

    placeholder

--- app/javascript/themes/default/contact.svelte
+++ app/javascript/themes/default/contact.svelte
@@ -1,1 +1,4 @@
-Contact form here
+
+<main class="container mt-50">
+Hier können Sie uns demnächst Nachrichten zukommen lassen.
+</main>
--- app/javascript/themes/default/faq.svelte
+++ app/javascript/themes/default/faq.svelte
@@ -1,1 +1,4 @@
-FAQ here
+
+<main class="container mt-50">
+  Häufig gestellte Fragen und Antworten werden Sie demächst hier finden.
+</main>
